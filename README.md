b84e5bae412dc42a4357fa78b150c1d97467da38
Infocat <infocat2.0@gmail.com>
2023-10-28 20:51:16 -0500

    0.2

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 Un block de notas pequeño y rapido para agregar un texto simple momentáneamente
 
 En codigo ejecutar: flet run mini.py -d
+
+Comandos usados: 
+python -m venv .venv
+python -m ensurepip --upgrade
+python -m pip install flet
+pip install flet
+
+Deploy:
+pip install pyinstaller
+flet pack mini.py
--- mini.py
+++ mini.py
@@ -5,7 +5,7 @@ import flet as ft
 def main(page: ft.Page):
 	page.window_center()
 	page.window_width=400
-	page.window_height = 180
+	page.window_height = 190
 	page.window_resizable=True
 	page.window_maximizable=False
 	page.theme_mode = ft.ThemeMode.DARK
@@ -16,16 +16,19 @@ def main(page: ft.Page):
 			page.window_destroy()
 
 	t = ft.Text(value="¡Nota rápida!", color="white")
+	tinfo = ft.Text(value="Por: carlos@infocat", style=ft.TextThemeStyle.BODY_SMALL)
 	mt = ft.TextField(
 		label="Contenido de la nota",
 		multiline=True,
 		value="",
 		min_lines=3,
-		border_color = ft.colors.AMBER_200
+		border_color = ft.colors.AMBER_200,
+		text_size=14
 		)
 	page.on_keyboard_event = on_keyboard
 	page.controls.append(t)
-	page.add(mt)
+	page.add(mt, tinfo)
+	mt.focus()
 	page.update()
 
 ft.app(target=main)
